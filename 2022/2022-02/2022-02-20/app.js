8d7138d7f771c048ecada4024defa02b432e2d84
zavans42 <zavans42@gmail.com>
2022-02-20 15:33:56 +0700

    add CC for 2022-02-20

---
+++ 2022/2022-02/2022-02-20/app.js
@@ -0,0 +1,11 @@
+// CHALLENGE
+
+/* 
+There is an object/class already created called MrFreeze. Mark this object as frozen so that 
+no other changes can be made to it.
+*/
+
+// ANSWER
+
+// mark the MrFreeze object instance as frozen
+Object.freeze(MrFreeze)
