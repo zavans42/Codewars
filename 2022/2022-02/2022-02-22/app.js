e3e93a2d8727c74f1c4f60e6f6bb316ee3b676e0
zavans42 <zavans42@gmail.com>
2022-02-22 15:44:37 +0700

    add CC for 2022-02-22

---
+++ 2022/2022-02/2022-02-22/app.js
@@ -0,0 +1,16 @@
+// CHALLENGE
+
+/* 
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+
+*/
+
+// ANSWER
+
+websites.push('codewars')
