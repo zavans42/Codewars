b9fbf3d70fb1a43dca100a91a5a16a9fcfb79ec6
zavans42 <zavans42@gmail.com>
2022-02-23 15:47:24 +0700

    add CC for 2022-02-23

---
+++ 2022/2022-02/2022-02-23/app.js
@@ -0,0 +1,25 @@
+// CHALLENGE
+
+/* 
+
+This function should return an object, but it's not doing what's intended. What's wrong?
+
+STARTING CODE
+
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return
+    results;
+}
+
+*/
+
+// ANSWER
+
+function mystery() {
+    var results = {
+        sanity: 'Hello'
+    };
+    return results
+}
