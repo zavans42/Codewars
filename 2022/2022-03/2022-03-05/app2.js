9f67dabc4a2dccbb44a84a0b0e5237977be2d448
zavans42 <zavans42@gmail.com>
2022-03-05 10:56:57 +0700

    add CC for 2022-03-05

---
+++ 2022/2022-03/2022-03-05/app2.js
@@ -0,0 +1,40 @@
+/* CHALLENGE
+
+Our friend Pac has finally decided to pursue a career in the coding industry.
+He is a newbie, he needs to learn properly.
+Therefore, Pac wants to apply for the worldwide famous -and very demanding-
+'C0d3r 1ns1d3' Academy for beginners.
+In order to join, Pac is required to solve a series of 3 exercises about 'Bug Fixes'.
+
+*/
+
+/* STARTING CODE
+
+function {
+	var : Math.random()
+		if (career <= 0.32) {
+			return = FrontEnd Developer
+		 else if (career <= 0.65) 
+			return : BackEnd Developer,
+		} else {
+			return 'Full-Stack Developer'
+		}
+
+yourFutureCareer();
+
+*/
+
+// ANSWER
+
+function yourFutureCareer() {
+    var career = Math.random()
+    if (career <= 0.32) {
+        return 'FrontEnd Developer'
+    } else if (career <= 0.65) {
+        return 'BackEnd Developer'
+    } else {
+        return 'Full-Stack Developer'
+    }
+}
+
+yourFutureCareer();
