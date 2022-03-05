37650dcde1e235ec3ea18eb7c78ea3ec7914417f
zavans42 <zavans42@gmail.com>
2022-03-05 10:55:02 +0700

    add CC for 2022-03-05

---
+++ 2022/2022-03/2022-03-05/app.js
@@ -0,0 +1,20 @@
+/* CHALLENGE
+
+You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. 
+Fix the function to make it return 32 without changing the number or the operators.
+
+*/
+
+/* STARTING CODE
+
+function orderOperations () {
+  return 2 + 2 * 2 + 2 * 2
+}
+
+*/
+
+// ANSWER
+
+function orderOperations() {
+    return (2 + 2) * (2 + 2) * 2
+}
