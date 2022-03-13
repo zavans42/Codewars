86e8f6b1739542dd3a1f49350e2b80dabb7ea0b1
zavans42 <zavans42@gmail.com>
2022-03-13 11:52:18 +0700

    add CC for 2022-03-13

---
+++ 2022/2022-03/2022-03-13/app2.js
@@ -0,0 +1,36 @@
+/* CHALLENGE
+
+Coding in function giveMeFive, function accept 1 parameter:obj, it's an object.
+
+You need to the traverse the obj, if the length of the object key equals to 5, then push the key value to the array (you need to define the array by yourself, this time I won't help you). Additionally push the value to the array as well, if the length of the value is equal to 5.
+
+Return the five after works finished.
+
+You should use for..in in your code, otherwise, your solution may not pass this kata. Don't learn bad habits from those lazy guys ;-)
+
+*/
+
+/* EXAMPLE
+
+giveMeFive({Our:"earth",is:"a",beautyful:"world"}) // ["earth","world"]
+giveMeFive({Ihave:"enough", money:"to",buy:"a",car:"model"}) // ["Ihave","money","model"]
+giveMeFive({Pears:"than",apple:"sweet"}) // ["Pears","apple","sweet"]
+
+*/
+
+// ANSWER
+
+function giveMeFive(obj) {
+    let five = [];
+
+    for (let key in obj) {
+        if (key.length === 5) {
+            five.push(key)
+        }
+        if (obj[key].length === 5) {
+            five.push(obj[key])
+        }
+    }
+
+    return five
+}
