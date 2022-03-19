c864e729641724d79280ea556b3fe59665f9f11e
zavans42 <zavans42@gmail.com>
2022-03-19 14:25:24 +0700

    add CC for 2022-03-19

---
+++ 2022/2022-03/2022-03-19/app3.js
@@ -0,0 +1,39 @@
+/* CHALLENGE
+
+Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database. 
+It should default to English if the language is not in the database, or in the event of an invalid input.
+
+*/
+
+
+/* EXAMPLE
+
+greet('english') // 'Welcome
+greet('dutch') // 'Welkom
+greet('IP_ADDRESS_INVALID') // 'Welcome
+
+*/
+
+// ANSWER
+
+const greet = lang => {
+    return lang === 'english' ? 'Welcome' :
+        lang === 'czech' ? 'Vitejte' :
+        lang === 'danish' ? 'Velkomst' :
+        lang === 'dutch' ? 'Welkom' :
+        lang === 'estonian' ? 'Tere tulemast' :
+        lang === 'finnish' ? 'Tervetuloa' :
+        lang === 'flemish' ? 'Welgekomen' :
+        lang === 'french' ? 'Bienvenue' :
+        lang === 'german' ? 'Willkommen' :
+        lang === 'irish' ? 'Failte' :
+        lang === 'italian' ? 'Benvenuto' :
+        lang === 'latvian' ? 'Gaidits' :
+        lang === 'lithuanian' ? 'Laukiamas' :
+        lang === 'polish' ? 'Witamy' :
+        lang === 'spanish' ? 'Bienvenido' :
+        lang === 'swedish' ? 'Valkommen' :
+        lang === 'welsh' ? 'Croeso' :
+        'Welcome'
+}   
