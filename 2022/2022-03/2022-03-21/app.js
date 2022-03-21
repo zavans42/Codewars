e5d8dcfcc1cf754f25a207bd9111ba5bb96269c3
zavans42 <zavans42@gmail.com>
2022-03-21 19:49:44 +0700

    add CC for 2022-03-21

---
+++ 2022/2022-03/2022-03-21/app.js
@@ -0,0 +1,73 @@
+/* CHALLENGE
+
+The function is not returning the correct values. Can you figure out why?
+
+*/
+
+/* STARTING CODE
+
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1:
+      name = 'Mercury'
+    case 2:
+      name = 'Venus'
+    case 3:
+      name = 'Earth'
+    case 4:
+      name = 'Mars'
+    case 5:
+      name = 'Jupiter'
+    case 6:
+      name = 'Saturn'
+    case 7:
+      name = 'Uranus'
+    case 8:
+      name = 'Neptune'
+  }
+  
+  return name;
+}
+
+// EXAMPLE
+
+getPlanetName(2) // 'Venus'
+getPlanetName(5) // 'Jupiter'
+getPlanetName(3) // 'Earth'
+
+*/
+
+// ANSWER
+
+function getPlanetName(id) {
+    var name;
+    switch (id) {
+        case 1:
+            name = 'Mercury'
+            break;
+        case 2:
+            name = 'Venus'
+            break;
+        case 3:
+            name = 'Earth'
+            break;
+        case 4:
+            name = 'Mars'
+            break;
+        case 5:
+            name = 'Jupiter'
+            break;
+        case 6:
+            name = 'Saturn'
+            break;
+        case 7:
+            name = 'Uranus'
+            break;
+        case 8:
+            name = 'Neptune'
+            break;
+    }
+
+    return name;
+}
