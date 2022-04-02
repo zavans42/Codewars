af0c6b26097b5e856e10f107adda0bd51f4233ce
zavans42 <zavans42@gmail.com>
2022-04-02 10:40:08 +0700

    add CC for 2022-04-02

---
+++ 2022/2022-04/2022-04-02/app2.js
@@ -0,0 +1,30 @@
+/* CHALLENGE
+
+Messi goal scoring statistics:
+    La Liga	43
+    Champions League	10
+    Copa del Rey	5
+
+Create these three variables and store the appropriate values using the table above:
+
+1. laLigaGoals
+2. championsLeagueGoals
+3. copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+*/
+
+
+/* EXAMPLE
+
+totalGoals // 58
+
+*/
+
+// ANSWER
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = 58
