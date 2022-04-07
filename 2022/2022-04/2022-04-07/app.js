26cf704a3398b9109b12d6e2aa59d4f97127d4ec
zavans42 <zavans42@gmail.com>
2022-04-07 10:57:10 +0700

    add CC for 2022-04-07

---
+++ 2022/2022-04/2022-04-07/app.js
@@ -0,0 +1,31 @@
+/* CHALLENGE
+
+Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+
+*/
+
+
+/* EXAMPLE
+
+summation(2) -> 3
+1 + 2
+
+summation(8) -> 36
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+*/
+
+// ANSWER
+
+var summation = function (num) {
+    // Code here
+
+    let result = 0;
+
+    for (let i = 1; i <= num; i++) {
+        result += i;
+    }
+
+    return result;
+
+}
