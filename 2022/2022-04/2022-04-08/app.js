ab16651760c9ef9d6503f2d74df811d9365e8c7f
zavans42 <zavans42@gmail.com>
2022-04-08 10:58:44 +0700

    add CC for 2022-04-08

---
+++ 2022/2022-04/2022-04-08/app.js
@@ -0,0 +1,18 @@
+/* CHALLENGE
+
+Make a simple function called greet that returns the most-famous "hello world!".
+
+*/
+
+
+/* EXAMPLE
+
+greet() // "hello world!"
+
+*/
+
+// ANSWER
+
+
+// Write a function "greet" that returns "hello world!"
+const greet = () => "hello world!"
