170a1bca15147987ecc35c163ab7dd0e999ac308
zavans42 <zavans42@gmail.com>
2022-04-15 11:42:47 +0700

    add CC for 2022-04-15

---
+++ 2022/2022-04/2022-04-15/app.js
@@ -0,0 +1,19 @@
+/* CHALLENGE
+
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+*/
+
+
+/* EXAMPLE
+
+boolToWord(true) // 'Yes'
+boolToWord(false) // 'No'
+
+*/
+
+// ANSWER
+
+function boolToWord(bool) {
+    return bool ? "Yes" : "No"
+}
