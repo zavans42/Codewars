c70fbf98a47aa9b4ef673f2385a82aa30526396f
zavans42 <zavans42@gmail.com>
2022-04-16 11:46:17 +0700

    add CC for 2022-04-16

---
+++ 2022/2022-04/2022-04-16/app.js
@@ -0,0 +1,18 @@
+/* CHALLENGE
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+*/
+
+
+/* EXAMPLE
+
+name // "codewa.rs"
+
+*/
+
+// ANSWER
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
