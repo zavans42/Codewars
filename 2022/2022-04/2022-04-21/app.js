eb0ad49438076026c12cb1c613e781d27ed83754
zavans42 <zavans42@gmail.com>
2022-04-21 14:20:28 +0700

    add CC for 2022-04-21

---
+++ 2022/2022-04/2022-04-21/app.js
@@ -0,0 +1,26 @@
+/* CHALLENGE
+
+You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+
+*/
+
+/* EXAMPLE
+
+updateLight("green") // "yellow"
+updateLight("yellow") // "red"
+updateLight("red") // "green"
+
+*/
+
+// ANSWER
+
+function updateLight(current) {
+
+    return current === 'green' ? 'yellow' :
+        current === 'yellow' ? 'red' :
+        'green'
+
+}
