e8aefa1352bc8c9ffb6dac44f7c44752648c32c2
zavans42 <zavans42@gmail.com>
2022-04-22 14:21:49 +0700

    add CC for 2022-04-22

---
+++ 2022/2022-04/2022-04-22/app.js
@@ -0,0 +1,15 @@
+/* CHALLENGE
+
+Write a function which converts the input string to uppercase.
+
+*/
+
+/* EXAMPLE
+
+makeUpperCase("hello") // "HELLO"
+
+*/
+
+// ANSWER
+
+const makeUpperCase = (str) => str.toUpperCase();
