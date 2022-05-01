84b1d48e099559aacd5c4f422fc192becabc7164
zavans42 <zavans42@gmail.com>
2022-05-01 10:42:51 +0700

    add CC for 2022-05-01

---
+++ 2022/2022-05/2022-05-01/app3.js
@@ -0,0 +1,21 @@
+/* CHALLENGE
+
+Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. 
+You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. 
+Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+*/
+
+/* EXAMPLE
+
+smash(["hello", "world"]) // "hello world"
+smash(["hello", "amazing", "world"]) // "hello amazing world"
+smash(["this", "is", "a", "really", "long", "sentence"]) // "this is a really long sentence"
+
+*/
+
+// ANSWER
+
+function smash(words) {
+    return words.join(' ')
+};
