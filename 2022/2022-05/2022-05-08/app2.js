3f53763adb0b7afdc10abd9d81955280e83dd01a
zavans42 <zavans42@gmail.com>
2022-05-08 13:21:00 +0700

    add CC for 2022-05-08

---
+++ 2022/2022-05/2022-05-08/app2.js
@@ -0,0 +1,21 @@
+/* CHALLENGE
+
+Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. 
+Write a function to help Bob with this calculation.
+
+*/
+
+/* EXAMPLE
+
+getVolumeOfCuboid(1,2,2) // 4
+getVolumeOfCuboid(6.3,2,5) // 63
+
+*/
+
+// ANSWER
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height
+    }
+}
