79383403ec259b215318bda71196919b2446d01e
zavans42 <zavans42@gmail.com>
2022-05-17 11:37:24 +0700

    add CC for 2022-05-17

---
+++ 2022/2022-05/2022-05-17/app.js
@@ -0,0 +1,46 @@
+/* CHALLENGE
+
+I have a cat and a dog.
+I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+Return their respective ages now as [humanYears,catYears,dogYears]
+
+NOTES:
+
+humanYears >= 1
+humanYears are whole numbers only
+
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
+
+*/
+
+/* EXAMPLE
+humanYearsCatYearsDogYears(1) // [1,15,15]
+humanYearsCatYearsDogYears(1) // [1,15,15]
+humanYearsCatYearsDogYears(10) // [10,56,64]
+
+*/
+
+// ANSWER
+
+var humanYearsCatYearsDogYears = (humanYears, catYears = 15, dogYears = 15) => {
+
+    if (humanYears > 1) {
+        dogYears += 9;
+        catYears += 9;
+        if (humanYears > 2) {
+            dogYears += (5 * (humanYears - 2));
+            catYears += (4 * (humanYears - 2));
+        }
+    }
+
+    return [humanYears, catYears, dogYears]
+}
