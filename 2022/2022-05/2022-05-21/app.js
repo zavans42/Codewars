2a3835abefe63dfdeaa7001ce85e664677506cdb
zavans42 <zavans42@gmail.com>
2022-05-21 11:48:39 +0700

    add CC for 2022-05-21

---
+++ 2022/2022-05/2022-05-21/app.js
@@ -0,0 +1,29 @@
+/* CHALLENGE
+
+Please refer to two example above and write your first JS function.
+
+mission 1:
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+type the console.log() in the next line(don't forget to put the str in the brackets).
+
+*/
+
+/* EXAMPLE
+
+helloWorld() // 'Hello World!'
+
+*/
+
+// ANSWER
+
+//refer to the example and write your first JS function
+function helloWorld() {
+    let str = 'Hello World!'
+
+    console.log(str)
+}
