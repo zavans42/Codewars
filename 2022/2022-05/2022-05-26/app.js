d8643e7f846e527dd8c5907e958af7f21a4e544c
zavans42 <zavans42@gmail.com>
2022-05-26 15:53:05 +0700

    add CC for 2022-05-26

---
+++ 2022/2022-05/2022-05-26/app.js
@@ -0,0 +1,18 @@
+/* CHALLENGE
+
+Code as fast as you can! You need to double the integer and return it.
+
+*/
+
+/* EXAMPLE
+
+doubleInteger(2) // 4
+
+*/
+
+// ANSWER
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2
+}
