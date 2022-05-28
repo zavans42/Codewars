6d76e5c465ec5c539a41784c738ca862a93c059d
zavans42 <zavans42@gmail.com>
2022-05-28 14:20:05 +0700

    add CC for 2022-05-28

---
+++ 2022/2022-05/2022-05-28/app2.js
@@ -0,0 +1,16 @@
+/* CHALLENGE
+
+Create a function named combineNames that accepts two parameters (first and last name). 
+The function should return the full name.
+
+*/
+
+/* EXAMPLE
+
+combineNames('James', 'Stevens') // 'James Stevens'
+
+*/
+
+// ANSWER
+
+const combineNames = (f, l) => `${f} ${l}`
