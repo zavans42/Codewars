bd0bdec40da3e6dc3950d238b493ddf509a2818d
zavans42 <zavans42@gmail.com>
2022-05-29 16:12:20 +0700

    add CC for 2022-05-29

---
+++ 2022/2022-05/2022-05-29/app.js
@@ -0,0 +1,16 @@
+/* CHALLENGE
+
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+*/
+
+/* EXAMPLE
+
+greet("Ryan") // "Hello, Ryan how are you doing today?"
+greet("Shingles") // "Hello, Shingles how are you doing today?"
+
+*/
+
+// ANSWER
+
+const greet = (name) => `Hello, ${name} how are you doing today?`
