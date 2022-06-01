c32bf27d5e273a0a8e84b8248246fef4dcc7b564
zavans42 <zavans42@gmail.com>
2022-06-01 08:40:31 +0700

    add CC for 2022-06-01

---
+++ 2022/2022-06/2022-06-01/app.js
@@ -0,0 +1,15 @@
+/* CHALLENGE
+
+Now you have to write a function that takes an argument and returns the square of it.
+
+*/
+
+/* EXAMPLE
+
+square(3) // 9
+
+*/
+
+// ANSWER
+
+const square = num => num ** 2
