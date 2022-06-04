d9901d877bc59d750d855f4439bd9118fb552471
zavans42 <zavans42@gmail.com>
2022-06-04 15:06:41 +0700

    add CC for 2022-06-04

---
+++ 2022/2022-06/2022-06-04/app2.js
@@ -0,0 +1,24 @@
+/* 
+
+# CHALLENGE
+
+Your task is to complete this Class, the Person class has been created. You must fill in the Constructor 
+method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info 
+getter which should return johns age is 34
+
+ 
+# EXAMPLE
+
+john.info // 'johns age is 34'
+
+*/
+
+// ANSWER
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.info = `${this.name}s age is ${this.age}`
+    }
+}
