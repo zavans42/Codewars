4b9466f01f03b450984b4883ddde6439d37ea7c2
zavans42 <zavans42@gmail.com>
2022-06-08 08:58:19 +0700

    add CC for 2022-06-08

---
+++ 2022/2022-06/2022-06-08/app.js
@@ -0,0 +1,16 @@
+/* 
+
+# CHALLENGE
+
+Write a function to split a string and convert it into an array of words.
+ 
+# EXAMPLE
+
+"Robin Singh" ==> ["Robin", "Singh"]
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+*/
+
+// ANSWER
+
+const stringToArray = str => str.split(' ')
