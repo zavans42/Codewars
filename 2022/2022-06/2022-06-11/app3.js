524aecf4dc39170108c08b5337ed72de5ef142e0
zavans42 <zavans42@gmail.com>
2022-06-11 15:15:49 +0700

    add CC for 2022-06-11

---
+++ 2022/2022-06/2022-06-11/app3.js
@@ -0,0 +1,25 @@
+/* 
+
+# CHALLENGE
+
+The function takes in 2 inputs x and y, and should return x to the power of y
+Simple, right? But you're NOT allowed to use Math.pow();
+Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+Note : zero to the power of zero equals one in this kata
+
+
+# EXAMPLE
+
+power(1,701270) // 1
+power(2,2) // 4
+power(3,2) // 9
+power(-1,40) // 1
+
+*/
+
+// ANSWER
+
+function power(x, y) {
+    return x ** y
+}
