d78c64631c8db1894cf639f119fe268f79b2ff2c
zavans42 <zavans42@gmail.com>
2022-06-13 09:06:37 +0700

    add CC for 2022-06-13

---
+++ 2022/2022-06/2022-06-13/app.js
@@ -0,0 +1,20 @@
+/* 
+
+# CHALLENGE
+
+Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. 
+Note that name will be an array consisting of one or more values that should be joined together with one space 
+between each, and the length of the name array in test cases will vary.
+ 
+# EXAMPLE
+
+sayHello(['John', 'Smith'], 'Phoenix', 'Arizona') // 'Hello, John Smith! Welcome to Phoenix, Arizona!'
+sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois') // 'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
+sayHello(['Wallace','Russel','Osbourne'],'Albany','New York') // 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
+*/
+
+// ANSWER
+
+function sayHello(name, city, state) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
