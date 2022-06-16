a5b10ef828c2a63d7d218735d881644bdb420d32
zavans42 <zavans42@gmail.com>
2022-06-16 09:12:07 +0700

    add CC for 2022-06-16

---
+++ 2022/2022-06/2022-06-16/app.js
@@ -0,0 +1,21 @@
+/* 
+
+# CHALLENGE
+
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+# EXAMPLE
+
+booleanToString(true) // "true", 'When we pass in true, we want the string "true" as output');
+booleanToString(false) // "false", 'When we pass in false, we want the string "false" as output');
+
+*/
+
+// ANSWER
+
+function booleanToString(b) {
+    // using toString() method
+    return b.toString();
+}
