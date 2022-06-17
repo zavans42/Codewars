c254b5758aca7bacdc478bff5a7d3b58255abcbe
zavans42 <zavans42@gmail.com>
2022-06-17 09:17:53 +0700

    add CC for 2022-06-17

---
+++ 2022/2022-06/2022-06-17/app.js
@@ -0,0 +1,17 @@
+/* 
+
+# CHALLENGE
+
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+# EXAMPLE
+
+binToDec(inp) // exp
+
+*/
+
+// ANSWER
+
+function binToDec(bin) {
+    return parseInt(bin, 2)
+}
