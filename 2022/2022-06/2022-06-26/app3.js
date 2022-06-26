03d948a9fd6b2046ea5f6c329717426575ebfb98
zavans42 <zavans42@gmail.com>
2022-06-26 09:56:43 +0700

    add CC for 2022-06-26

---
+++ 2022/2022-06/2022-06-26/app3.js
@@ -0,0 +1,22 @@
+/* 
+
+# CHALLENGE
+
+This method, which is supposed to return the result of dividing its first argument by its second, 
+isn't always returning correct values. Fix it.
+
+# EXAMPLE
+
+solve(2, 1) // 2
+solve(2, 4) // 0.5
+solve(42, 0) // Infinity
+solve(0, 8) // 0
+solve(7, 2) // 3.5
+solve(4.2, 1) // 4.2
+solve(9999, 11) // 909
+
+*/
+
+// ANSWER
+
+const solve = (x, y) => x / y
