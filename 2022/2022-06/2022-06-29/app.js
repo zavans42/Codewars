cb03d1c1307a14ee4db1224b80883ddfc23b014b
zavans42 <zavans42@gmail.com>
2022-06-29 10:39:54 +0700

    add CC for 2022-06-29

---
+++ 2022/2022-06/2022-06-29/app.js
@@ -0,0 +1,24 @@
+/* 
+
+# CHALLENGE
+
+This code does not execute properly. Try to figure out why.
+
+# EXAMPLE
+
+function multiply(a, b){
+  a * b
+}
+
+multiply(1,1) // 1
+multiply(2,1) // 2
+multiply(2,2) // 4
+multiply(3,5) // 15   
+
+*/
+
+// ANSWER
+
+function multiply(a, b) {
+    return a * b
+}
