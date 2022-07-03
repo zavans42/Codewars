979a36cbc99e28bdadb21822ea055b67e234920c
zavans42 <zavans42@gmail.com>
2022-07-03 11:56:57 +0700

    add CC for 2022-07/03

---
+++ 7kyu/2022-07/2022-07-03/app3.js
@@ -0,0 +1,29 @@
+/* 
+
+# CHALLENGE
+
+You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return either:
+1. true if all developers in the list code in the same language; or
+2. false otherwise.
+
+# EXAMPLE
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+  { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+];
+
+your function should return true.
+
+*/
+
+// ANSWER
+
+function isSameLanguage(list) {
+    return list.every(dev => dev.language === list[0].language)
+}
