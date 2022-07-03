8bd57ce9c361ca8a6aa139a1d487821574f1e652
zavans42 <zavans42@gmail.com>
2022-07-03 11:15:51 +0700

    add CC for 2022-07/03

---
+++ 7kyu/2022-07/2022-07-03/app.js
@@ -0,0 +1,42 @@
+/* 
+
+# CHALLENGE
+
+You will be given an array of objects (associative arrays in PHP, table in COBOL) representing data about developers who have 
+signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an object (associative array in PHP, table in COBOL) which includes the count of each coding language represented at the meetup.
+
+ 
+# EXAMPLE
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'C' },
+  { firstName: 'Anna', lastName: 'R.', country: 'Liechtenstein', continent: 'Europe', age: 52, language: 'JavaScript' },
+  { firstName: 'Ramon', lastName: 'R.', country: 'Paraguay', continent: 'Americas', age: 29, language: 'Ruby' },
+  { firstName: 'George', lastName: 'B.', country: 'England', continent: 'Europe', age: 81, language: 'C' },
+];
+
+your function should return the following object (associative array in PHP, table in COBOL):
+{ C: 2, JavaScript: 1, Ruby: 1 }
+
+*/
+
+// ANSWER
+
+function countLanguages(list) {
+    // thank you for checking out the Coding Meetup kata :)
+    const languageList = list.map(x => x.language)
+
+    const count = languageList.reduce(function (obj, item) {
+        if (!obj[item]) {
+            obj[item] = 0;
+        }
+        obj[item]++;
+        return obj;
+    }, {});
+
+    return count
+}
