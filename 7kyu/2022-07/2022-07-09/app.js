5e606e30abdbecee05221a1c7a9f619c8dfb8aae
zavans42 <zavans42@gmail.com>
2022-07-09 11:32:26 +0700

    add CC for 2022-07/09

---
+++ 7kyu/2022-07/2022-07-09/app.js
@@ -0,0 +1,19 @@
+/* 
+
+# CHALLENGE
+
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+# EXAMPLE
+
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+*/
+
+// ANSWER
+
+var flatten = function (array) {
+    return array.reduce((a, b) => a.concat(b), [])
+}
