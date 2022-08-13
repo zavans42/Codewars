6ffecfc959d6c237f60af9c51450d8b02fb420a0
zavans42 <zavans42@gmail.com>
2022-08-13 08:32:30 +0700

    add CC for 2022-08-13

---
+++ 6kyu/2022-08-13/app.js
@@ -0,0 +1,36 @@
+/* 
+
+### CHALLENGE
+
+You will be given a sequence of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if all of the following continents / geographic zones will be represented by at least one developer: 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'.
+false otherwise.
+
+### EXAMPLE
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+];
+
+your function should return true as there is at least one developer from the required 5 geographic zones.
+
+*/
+
+// ANSWER
+
+function allContinents(list) {
+    const continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+    const devContinentsArr = list.map(x => x.continent)
+
+    return continents.every(x => devContinentsArr.includes(x))
+
+}
