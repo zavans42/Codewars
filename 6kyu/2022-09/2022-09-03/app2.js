f100560fe155fc7315af115ffa2877853be8c38a
zavans42 <zavans42@gmail.com>
2022-09-03 08:57:56 +0700

    add CC for 2022-09-03

---
+++ 6kyu/2022-09/2022-09-03/app2.js
@@ -0,0 +1,27 @@
+/* 
+
+### Count the smiley faces!
+
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+> Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+> A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+> Every smiling face must have a smiling mouth that should be marked with either ) or D
+
+No additional characters are allowed except for those mentioned.
+
+
+### Example
+
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+*/
+
+// ANSWER
+
+function countSmileys(arr) {
+  return arr.filter((x) => /^[:;][-~]?[)D]$/.test(x)).length;
+}
