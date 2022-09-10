01690bcc013a65de6defebd81ec38f3289e106f4
zavans42 <zavans42@gmail.com>
2022-09-10 10:01:18 +0700

    add CC for 2022-09-10

---
+++ 6kyu/2022-09/2022-09-10/app.js
@@ -0,0 +1,34 @@
+/* 
+
+### Meeting
+
+John has invited some friends. His list is:
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+
+Could you make a program that
+1. makes this string uppercase
+2. gives it sorted in alphabetical order by last name.
+When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+So the result of function meeting(s) will be:
+"(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+
+### Example
+
+s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+
+>>> "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+
+
+*/
+
+// ANSWER
+
+const meeting = (s) => {
+  return s
+    .toUpperCase()
+    .replace(/(\w+):(\w+)/g, "($2, $1)")
+    .split(";")
+    .sort()
+    .join("");
+};
