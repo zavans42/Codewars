0cc1a3032eeeb45da5c1a77dcc166d5453cf0de9
zavans42 <zavans42@gmail.com>
2022-10-01 08:23:36 +0700

    add CC for 2022-10-01

--- 5kyu/2022-10/2022-10-01/app.js
+++ 5kyu/2022-10/2022-10-01/app.js
@@ -21,12 +21,13 @@ pigIt('Hello world !');     // elloHay orldway !
 */
 
 function pigIt(str) {
-  const arr = str.split(" ");
-  return arr
-    .map((word) => {
-      return word.match(/[A-z]/i)
-        ? `${word.substr(1)}${word.substr(0, 1)}ay`
-        : word;
-    })
-    .join(" ");
-}
+    const arr = str.split(" ");
+    return arr
+      .map((word) => {
+        return word.match(/[A-z]/i)
+          ? `${word.substr(1)}${word.substr(0, 1)}ay`
+          : word;
+      })
+      .join(" ");
+  }
+  
