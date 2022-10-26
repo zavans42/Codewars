e07e7a5988b08ce76fc815919e2e77b294d6db4e
zavans42 <zavans42@gmail.com>
2022-10-26 09:34:29 +0700

    add CC for 2022-10-26

---
+++ 5kyu/2022-10/2022-10-26/app.js
@@ -0,0 +1,33 @@
+/*
+
+What's a Perfect Power anyway?
+Tags: MATHEMATICS, FUNDAMENTALS
+
+1. DESCRIPTION
+
+A perfect power is a classification of positive integers:
+In mathematics, a perfect power is a positive integer that can be expressed as an integer power of another positive integer. More formally, n is a perfect power if there exist natural numbers m > 1, and k > 1 such that mk = n.
+
+Your task is to check wheter a given integer is a perfect power. If it is a perfect power, return a pair m and k with mk = n as a proof. Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
+
+Note: For a perfect power, there might be several pairs. For example 81 = 3^4 = 9^2, so (3,4) and (9,2) are valid solutions. However, the tests take care of this, so if a number is a perfect power, return any pair that proves it.
+
+
+2. EXAMPLE
+
+Test.describe("perfect powers", function(){
+  Test.it("should work for some examples",function(){
+    Test.assertSimilar(isPP(4), [2,2], "4 = 2^2");
+    Test.assertSimilar(isPP(9), [3,2], "9 = 3^2");
+    Test.assertEquals( isPP(5), null, "5 isn't a perfect number");
+  });
+});
+
+*/
+
+const isPP = function (n) {
+  for (let m = 2; m * m <= n; m++)
+    for (let k = 2, guess = m * m; guess <= n; k++, guess *= m)
+      if (guess == n) return [m, k];
+  return null;
+};
